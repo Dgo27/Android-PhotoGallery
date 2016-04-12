30ffc41a6fdf10de68913fd38f6d72d6a652df97
Dgo27 <diegoverona27@gmail.com>
2016-04-13 01:53:23 +0200

    - Create link with google with APIs - Created new activity with toolbar and google maps - Updated images of the first screen

--- app/src/main/java/verona/diego/photogallery/MainActivity.java
+++ app/src/main/java/verona/diego/photogallery/MainActivity.java
@@ -1,17 +1,51 @@
 package verona.diego.photogallery;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton btn_search_by_address;
+    ImageButton btn_search_by_map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar_main);
         setSupportActionBar(myToolbar);
         getSupportActionBar();
+
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+
+        btn_search_by_address = (ImageButton) findViewById(R.id.action_search_by_address);
+        btn_search_by_map = (ImageButton) findViewById(R.id.action_search_by_map);
+
+        btn_search_by_address.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Toast.makeText(MainActivity.this,
+                        "Click! ahah", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+        btn_search_by_map.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                startActivity(new Intent(MainActivity.this,SearchByMapActivity.class));
+            }
+
+        });
     }
 }
---
+++ app/src/main/java/verona/diego/photogallery/SearchByMapActivity.java
@@ -0,0 +1,75 @@
+package verona.diego.photogallery;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class SearchByMapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_by_map);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar_map);
+        setSupportActionBar(myToolbar);
+        getActionBar();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search_by_map, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            case R.id.action_search_address:
+                Toast.makeText(SearchByMapActivity.this,
+                        "Search!", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_current_position:
+                Toast.makeText(SearchByMapActivity.this,
+                        "Get current position!", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+}
